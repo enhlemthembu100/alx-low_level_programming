96f5300d540aca59013a1a211d44cf4f82c29d86
enhlemthembu100 <enhlemthembu100@icloud.com>
2023-08-10 16:36:34 -0700

    Hello world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,27 @@
-#include<stdio.h>
+#include "main.h"
+
 /**
- * * main - Entry point
- * *
- * * Return: Always 0 (Success)
- * */
-int main(void)
+ * print_sign - prints the sign of a number
+ * @n: the int to check
+ * Return: 1 and prints + if n is greater than zero
+ * 0 and prints 0 if n is zero
+ * -1 and prints - if n is less than zero
+ */
+int print_sign(int n)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n")
-return (0);
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n == 0)
+}
+		_putchar(48);
+		return (0);
+	} else if (n < 0)
+	{
+		_putchar('-');
+	}
+		return (-1);
+}
+
 }
